3983111cebd0b685c8ae00893b461c2f62819363
Efeglas <gabor.atty@gmail.com>
2023-07-19 19:09:42 +0200

    Added validation functions
    Added longer than x number validation
    Added contains number validation
    Added contains uppercase validation
    Added password validation (combination of prev. three)

--- src/lib/helpers/Validator.ts
+++ src/lib/helpers/Validator.ts
@@ -3,4 +3,24 @@ export class Validator {
     static notEmpty = (value: string) : boolean => {
         return value.trim() !== "";
     }
+
+    static longerThan = (value: string, min: number) : boolean => {
+        return value.trim().length > min;
+    }
+
+    static containsNumber = (value: string) : boolean => {
+        return /\d/.test(value);
+    }
+
+    static containsUpperCase = (value: string) : boolean => {
+        return /[A-Z]/.test(value);
+    }
+
+    static password = (value: string) : boolean => {
+        return this.longerThan(value, 7) && this.containsNumber(value) && this.containsUpperCase(value);
+    }
+
+    static passwordPartial = (value: string) : Array<boolean> => {
+        return [this.longerThan(value, 7), this.containsNumber(value), this.containsUpperCase(value)];
+    }
 }
