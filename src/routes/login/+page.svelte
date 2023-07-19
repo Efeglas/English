ddf780dbc89783a09607d7577da2e29919934b81
Efeglas <gabor.atty@gmail.com>
2023-07-19 19:07:35 +0200

    CORS and response handling
    Tuned CORS settings to work with cors backend
    Response handling of the login fetch

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -37,25 +37,40 @@
 
             let response = await fetch('https://localhost:7230/api/User/Login', {
                 method: "POST",
+                mode: "cors",
+                credentials: "include",
                 headers: {
-                    "Content-Type": "application/json",
+                    "Content-Type": "application/json",      
                 },
                 body: JSON.stringify({username: username, password: password})
             });
-            const result = await response.json();
-            console.log(result);
 
-            const t: ToastSettings = {
-                message: 'Successful login',
-                background: 'variant-filled-success',
-            };
+            let t: ToastSettings;
+            if (response.status === 200) {              
+                t = {
+                    message: 'Successful login',
+                    background: 'variant-filled-success',
+                };
+            } else {
+                t = {
+                    message: response.statusText,
+                    background: 'variant-filled-error',
+                };
+            }
             toastStore.trigger(t);
+
         } else {
             const t: ToastSettings = {
                 message: 'All fields must be filled',
                 background: 'variant-filled-error',
             };
             toastStore.trigger(t);
+        }
+    }
+
+    const enterPresshandler = (event) => {
+        if (event.keyCode === 13) {
+            submitLogin();
         }
     }
 
@@ -66,14 +81,15 @@
         <h1 class="text-center">Log In</h1>
         <label class="label">
             <span>Username</span>
-            <input bind:value={username} on:input={usernameTouchedHandler} on:blur={usernameTouchedHandler} class={usernameInputClass} type="text" placeholder="Username..." />
+            <input bind:value={username} on:input={usernameTouchedHandler} on:blur={usernameTouchedHandler} on:keydown={enterPresshandler} class={usernameInputClass} type="text" placeholder="Username..." />
         </label>
         <label class="label">
             <span>Password</span>
-            <input bind:value={password} on:input={passwordTouchedHandler} on:blur={passwordTouchedHandler} class={passwordInputClass} type="password" placeholder="Password..." />
+            <input bind:value={password} on:input={passwordTouchedHandler} on:blur={passwordTouchedHandler} on:keydown={enterPresshandler} class={passwordInputClass} type="password" placeholder="Password..." />
         </label>
         <div class="flex justify-center mt-6">
             <button on:click={submitLogin} type="button" class="btn variant-filled">Log In</button>
         </div>
+        <p class="text-center mt-2">I dont't have an account! <a href="/register" class="no-underline hover:underline font-bold">Register</a></p>
     </div>
 </div>
