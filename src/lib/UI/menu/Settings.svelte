eff725965d2c44d7abcb91876d254705c3f46204
Efeglas <gabor.atty@gmail.com>
2023-07-23 21:01:48 +0200

    Added logout
    Added logout button.
    Added logout function to handle process.

--- src/lib/UI/menu/Settings.svelte
+++ src/lib/UI/menu/Settings.svelte
@@ -1,9 +1,49 @@
 <script lang='ts'>
     import Fa from 'svelte-fa/src/fa.svelte';
-	import { faTimes } from '@fortawesome/free-solid-svg-icons';
-    import { LightSwitch } from '@skeletonlabs/skeleton';
+	import { faTimes, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+    import { LightSwitch, toastStore, type ToastSettings, drawerStore } from '@skeletonlabs/skeleton';
+    import {username as storeUsername} from '$lib/stores/user';
+	import { goto } from '$app/navigation';
 
     export let onClick: MouseEventHandler<HTMLButtonElement>;
+
+    const logoutHandler = async () => {
+        let response;
+            let t: ToastSettings;
+            try {
+                
+                response = await fetch('http://192.168.50.188:7230/api/User/Logout', {
+                    method: "POST",
+                    mode: "cors",
+                    credentials: "include",
+                    headers: {
+                        "Content-Type": "application/json",      
+                    },                  
+                });
+
+                if (response.status === 200) {              
+                    t = {
+                        message: 'Successful logout',
+                        background: 'variant-filled-success',
+                    };
+                    storeUsername.set(null);
+                    goto("/login");
+                    drawerStore.close();
+                } else {
+                    t = {
+                        message: 'Invalid username or password',
+                        background: 'variant-filled-error',
+                    };
+                }
+                toastStore.trigger(t);
+            } catch (error: any) {
+                t = {
+                    message: error,
+                    background: 'variant-filled-error',
+                };
+                toastStore.trigger(t);
+            }
+    }
 </script>
 
 <div class="flex justify-start p-6">
@@ -14,8 +54,9 @@
     <hr>
 </div>
 <div class="flex flex-col p-4">
-    <div class="flex justify-between">
+    <div class="flex justify-between mb-6">
         <span>Mode</span>
         <LightSwitch />
     </div>
+    <button on:click={logoutHandler} class="btn bg-initial hover:bg-surface-900"><Fa class='mr-1' icon={faRightFromBracket} /> Log Out</button>
 </div>
