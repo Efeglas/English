b130504dbaba0e49c2790bbeadcd70a677eef19c
Efeglas <gabor.atty@gmail.com>
2023-07-25 19:20:11 +0200

    Added placeholder Under Construction card

---
+++ src/lib/UI/cards/UnderConstruction.svelte
@@ -0,0 +1,10 @@
+<script lang='ts'>
+	import { faPersonDigging } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa/src/fa.svelte";   
+</script>
+
+<div class="p-6">
+    <div class="card">
+        <section class="p-4 flex flex-col justify-center"><Fa class='mr-1 text-5xl' icon={faPersonDigging} /><h1 class="text-center text-xl mt-4">Under Construction!</h1></section>
+    </div>
+</div>
